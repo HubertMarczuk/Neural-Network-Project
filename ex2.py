ce52e92c9572abd1e1443b4c6171cbbb97a81a01
HubertMarczuk <hubert.marczuk1@gmail.com>
2024-04-22 17:42:29 +0200

    ex3: fixed Fprime Function
    ex2:
    fixed history saving

--- ex2.py
+++ ex2.py
@@ -1,45 +1,53 @@
 def GenerateVectors(v_values, length):
     v = [[0 for i in range(length)] for j in range(pow(len(v_values),length))]
-    print(len(v), length)
     for i in range(length):
         for j in range(len(v)):
             v[j][length-(i+1)] = v_values[int((j/int(pow(len(v_values),i)))%len(v_values))]
     return v
 
 
-
-def CheckStabilization(weightmatrix):
-    for i in range(len(weightmatrix[0])):
-        if weightmatrix[i][i]==0:
+def CheckStabilization(weight):
+    for i in range(len(weight[0])):
+        if weight[i][i]==0:
             print("Warunek 1) niespełniony. Macierz wag nie ma wartości 0 na wszystkich elementach diagonali!!!")
             break
-    for i in range(len(weightmatrix[0])):
+    for i in range(len(weight[0])):
         for j in range(i):
-            if weightmatrix[i][j]!=weightmatrix[j][i]:
+            if weight[i][j]!=weight[j][i]:
                 print("Warunek 2) niespełniony. Macierz wag nie jest macierzą symetryczną!!!")
                 break
 
 
-def Synchronous(weightmatrix, v, threshold, activation_type, low_value, high_value):
+def Synchronous(weight, v, threshold, activation_type, low_value, high_value):
+    v_histories = []
+    for i in range(len(v)):
+        v_histories.append(Synchronous_a(weight, v[i], threshold, activation_type, low_value, high_value))
+    return v_histories
+
+def Synchronous_a(weight, v, threshold, activation_type, low_value, high_value):
     v_history = []
     v_history.append(v)
     while True:
-        u = MatrixMultipliesvector(weightmatrix,v)
+        u = MatrixMultipliesvector(weight,v)
         for i in range(len(u)):
             v[i] = ActivationFunction(u[i], threshold, activation_type, low_value, high_value)
         v_history.append(v)
         stop = False
         for i in range(len(v_history)-1):
-            if v == v_history[i]:
+            repeat = True
+            for j in range(len(v)):
+                if v[j] != v_history[i][j]:
+                    repeat = False
+                    break
+            if len(v_history) == 1:
+                repeat = False
+            if repeat == True:
                 stop = True
                 break
         if stop == True:
             break
     return v_history
         
-            
-
-
 def Asynchronous():
     k=0
 
@@ -48,7 +56,7 @@ def MatrixMultipliesvector(M,v):
     for i in range(len(v)):
         sum = 0
         for j in range(len(v)):
-            sum+=M[i][j]*u[j]
+            sum+=M[i][j]*v[j]
         u.append(sum)
     return u
 
@@ -64,14 +72,29 @@ def ActivationFunction(x, threshold, activation_type, low_value, high_value):
         else:
             return high_value
         
+def PrintHistories(v_histories):
+    for i in range(len(v_histories)):
+        for j in range(len(v_histories[0])):
+            print(v_histories[i][j])
+        print()
+        
 
 weight = [[0, -1, 1],
           [-1, 0, 0.5],
           [1, 0.5, 0]]
+
 v_values = [0,1]
+
 v = GenerateVectors(v_values,len(weight[0]))
+
 threshold = 0
-activation_type = True                  #True(< and >=), False(<= >)
+
+activation_type = True                  #True(< and >=), False(<= and >)
+
 low_value = 0
+
 high_value = 1
+
 #CheckStabilization(weight)
+sync = Synchronous(weight, v, threshold, activation_type, low_value, high_value)
+PrintHistories(sync)
--- ex3.py
+++ ex3.py
@@ -4,14 +4,8 @@ import matplotlib.pyplot as plt
 def F(t):
     return 1/(1+exp(-1*t))
 
-#def F(t):
-#    return (1-exp(-1*t))/(1+exp(-1*t))
-
-#def Fprime(t):
-#    return 2*exp(t)/pow(exp(t)+1,2)
-
-def Fprime(t):
-    return exp(t)/((exp(t)+1)*(exp(t)+1))
+def Fprime(x):
+    return x*(1-x)
 
 def E(d,y):
     return (d-y)*(d-y)
@@ -46,7 +40,7 @@ def TotalEnergy(w, x1, d, eps, alfa, mode, iterations):
         energy_hist.append(energy_sum)
         for i in range(3):
             for j in range(3):
-                w[i][j] = w[i][j] +grad_sum[i][j]
+                w[i][j] = w[i][j] - alfa * grad_sum[i][j]
         counter += 1
         if mode == True:
             if energy_sum <= eps:
@@ -68,20 +62,17 @@ def PartialEnergy(w, x1, d, eps, alfa, mode, iterations):
                 for j in range(3):
                     sum+=w[i][j]*x1[k][j]
                 x2.append(F(sum))
-            #print(x2)
             x3 = 0
             sum = 0
             for i in range(3):
                 sum += w[2][i]*x2[i]
             x3 = F(sum)
-            #print(x3)
             energy = E(d[k],x3)
             energy_sum += energy
             grad = Propagation(w, x1[k], x2, x3, d[k])
             for i in range(3):
                 for j in range(3):
-                    w[i][j] = w[i][j] +grad[i][j]
-            #print(w)
+                    w[i][j] = w[i][j] - alfa * grad[i][j]
         counter +=1
         energy_hist.append(energy_sum)
         if mode == True:
@@ -92,32 +83,17 @@ def PartialEnergy(w, x1, d, eps, alfa, mode, iterations):
                 break
     return energy_hist
 
-# def Propagation(w, x1, x2, x3, d):
-#     dx3 = Eprime(d,x3)
-#     dw3 = []
-#     for i in range(3):
-#         dw3.append(dx3*Fprime(x3)*x2[i])
-#     dw21 = []
-#     for i in range(3):
-#         dw21.append(dx3*Fprime(x3)*w[2][1]*Fprime(x2[1])*x1[i])
-#     dw22 = []
-#     for i in range(3):
-#         dw22.append(dx3*Fprime(x3)*w[2][2]*Fprime(x2[2])*x1[i])
-#     return [dw21, dw22, dw3]
-
 def Propagation(w, x1, x2, x3, d):
-    sigma3 = x3*(1-x3)*(d-x3)
-    sigma21 = x2[1]*(1-x2[1])*w[2][1]*sigma3
-    sigma22 = x2[2]*(1-x2[2])*w[2][2]*sigma3
+    dx3 = Eprime(d,x3)
     dw3 = []
     for i in range(3):
-        dw3.append(sigma3*x2[i]*0.5)
+        dw3.append(dx3*Fprime(x3)*x2[i])
     dw21 = []
     for i in range(3):
-        dw21.append(sigma21*x1[i]*0.5)
+        dw21.append(dx3*Fprime(x3)*w[2][1]*Fprime(x2[1])*x1[i])
     dw22 = []
     for i in range(3):
-        dw22.append(sigma22*x1[i]*0.5)
+        dw22.append(dx3*Fprime(x3)*w[2][2]*Fprime(x2[2])*x1[i])
     return [dw21, dw22, dw3]
 
 def Plot(energy):
@@ -152,8 +128,8 @@ eps_mode = False
 
 iterations = 6000
 
-energy_part = PartialEnergy(w, x1, d, eps, alfa, eps_mode, iterations)
-#energy_total = TotalEnergy(w, x1, d, eps, alfa, eps_mode, iterations)
+#energy_part = PartialEnergy(w, x1, d, eps, alfa, eps_mode, iterations)
+energy_total = TotalEnergy(w, x1, d, eps, alfa, eps_mode, iterations)
 
-Plot(energy_part)
-#Plot(energy_total)
+#Plot(energy_part)
+Plot(energy_total)
